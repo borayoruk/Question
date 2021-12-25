ce06c9b765fec1b8fd7c22afe53b7928ed233460
Selin Doga Orhan <73192627+selindoga@users.noreply.github.com>
2021-12-25 19:48:30 +0300

    added text input field for email

--- lib/main.dart
+++ lib/main.dart
@@ -9,58 +9,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Login Screen'),
+      home: InputEmail().build(context),
     );
   }
 }
 
 // logo ekleyebiliriz
 
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class InputEmail {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+    return const Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Padding(
+          padding:
+          EdgeInsetsDirectional.fromSTEB(
+            54,45,54,80
+          ),
+            child: TextField(
+              style: TextStyle(fontSize: 18),
+            keyboardType: TextInputType.emailAddress,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
+
   }
 }
